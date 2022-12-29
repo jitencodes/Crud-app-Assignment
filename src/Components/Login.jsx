ecf43208ae94cbbc9fd4099fd3b17461322d5547
Jeet <itsjeetusaini@gmail.com>
2022-12-29 11:33:37 +0530

    first commit

---
+++ src/Components/Login.jsx
@@ -0,0 +1,136 @@
+import React, {useState} from 'react';
+import "../index.css";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+const Login = () => {
+  // const BaseUrl = "https://authapi.onrender.com";
+  const Navigate = useNavigate();
+  
+  // constants
+  const [login, setLogin] = useState("login");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [location, setLocation] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // functions here!
+  const randomFixedInteger = (length)=> {
+    return Math.floor(Math.pow(10, length-1) + Math.random() * (Math.pow(10, length) - Math.pow(10, length-1) - 1));
+  }
+
+  const  handleSubmit = async (submitType)=>{
+    if(submitType=='login'){
+        //  VALIDATING INPUTS AND TRY LOGIN
+        if(email==''||password==''){
+          window.alert(`All Fields Are Required!`);
+        }else{
+          if(localStorage){
+            console.log('have')
+          }else{
+            console.log("don't have")
+          }
+          // Login QUERY
+          try{
+            const data  = {email,password};
+            const res = await axios.post(`/auth/login`,data);
+            // console.log(res.data);
+            if(typeof(res.data)==="string"){
+              window.alert(res.data);
+              setLogin('register');
+            }else{
+              localStorage.setItem("user", JSON.stringify(res.data));
+              handleResetStates();
+              Navigate('/');
+            }
+          }catch(error){
+            window.alert(error);
+          }
+        }
+    }else{
+    //  VALIDATING INPUTS AND TRY REGISTRATION
+      if(firstName==''||lastName==''|| email==''||password==''){
+        window.alert(`All Fields Are Required!`);
+      }else{
+        // REGISTER QUERY
+        try{
+        let employeeId = randomFixedInteger(6);
+        const data  = {firstName,lastName,location,employeeId,email,password};
+          const res = await axios.post(`/auth/register`,data);
+          if(typeof(res.data)==="string"){
+            window.alert(res.data);   
+          } 
+        }catch(error){
+          window.alert(error);
+        }
+      }
+      setLogin('login');
+      handleResetStates();
+    }
+  }
+
+  // reset const values
+  const handleResetStates=()=>{
+    setFirstName("");
+    setLastName("");
+    setLocation("");
+    setEmail("");
+    setPassword("");
+  }
+
+  return (
+    <div className='loginScreen'>
+      <div className="LoginLeft"></div>
+      <div className="LoginRight">
+        <div className="form-wrapper">
+          {
+            login=="login" ? (
+
+              // LOGIN FORM
+            <form action="" onSubmit={(e)=>{e.preventDefault(); handleSubmit(login)}}>
+            <h2 className="title">Login!</h2>
+            <h6>Enter your credentials to login to your account.</h6>
+            <div className="form-inputs">
+            <label htmlFor="email">Email</label>
+              <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+              <label htmlFor="password">Password</label>
+              <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+            </div>
+            <button type='submit' >Submit</button>
+            <h6>create a new account <span onClick={()=>{setLogin("register"); handleResetStates()}}>here!</span></h6>
+          </form>):(
+
+            // REGISTRATION FORM
+            <form action="" onSubmit={(e)=>{e.preventDefault(); handleSubmit(login)}}>
+            <h2 className="title">Register!</h2>
+            <h6>Fill in the following details to create new account.</h6>
+            <div className="form-inputs">
+            <div className="inputRow">
+            <div className="inputcell">
+            <label htmlFor="FirstName">First Name</label>
+              <input type="text" value={firstName} onChange={(e)=>{setFirstName(e.target.value)}} />
+            </div>
+            <div className="inputcell">
+            <label htmlFor="LastName">Last Name</label>
+              <input type="text" value={lastName} onChange={(e)=>{setLastName(e.target.value)}} />
+            </div>
+            </div>
+              <label htmlFor="location">Location</label>
+              <input type="text" value={location} onChange={(e)=>{setLocation(e.target.value)}}/>
+              <label htmlFor="email">Email</label>
+              <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+              <label htmlFor="password">Password</label>
+              <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+            </div>
+            <button type='submit'>Submit</button>
+            <h6>To log in your account click <span onClick={()=>{setLogin("login"); handleResetStates()}}>here!</span></h6>
+          </form>
+          )
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
